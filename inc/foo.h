c3c8675219fb801182d936d7d045e89934b9dfa3
Alexander Dahl <post@lespocky.de>
2012-03-06 13:54:34 +0100

    initial

---
+++ inc/foo.h
@@ -0,0 +1,28 @@
+/*******************************************************************//**
+ *	@file	foo.h
+ *
+ *	foo library header
+ *
+ *	@author	Alexander Dahl <post@lespocky.de>
+ *	@date	Created: 2012-03-06
+ **********************************************************************/
+
+#ifndef _FOO_H_
+#define _FOO_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ *	Just say hello from this lib!
+ */
+void foo_hello( void );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* _FOO_H_ */
+
+/* vim: set noet sts=0 ts=4 sw=4 sr: */
---
+++ src/foo.c
@@ -0,0 +1,18 @@
+/*******************************************************************//**
+ *	@file	foo.c
+ *
+ *	foo library implementation
+ *
+ *	@author	Alexander Dahl <post@lespocky.de>
+ *	@date	Created: 2012-03-06
+ **********************************************************************/
+
+#include "foo.h"
+
+#include <stdio.h>
+
+void foo_hello( void ) {
+	(void) puts( "This is foo library speaking." );
+}
+
+/* vim: set noet sts=0 ts=4 sw=4 sr: */
